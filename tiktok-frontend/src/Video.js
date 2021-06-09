f3cac3ac72522c9dc7ee0b0b64d0e5a443f61974
yash621 <yg17381@gmail.com>
2021-06-09 21:10:02 +0530

    playing feature added

--- tiktok-frontend/src/Video.js
+++ tiktok-frontend/src/Video.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
   return (
     <div className="video">
       <video
         loop
+        onClick={handleVideoPress}
+        ref={videoRef}
         className="video_player"
-        src="https://v39-eu.tiktokcdn.com/3f5f4e23696a1f130eda7dd9a78cb6f5/60bfbac4/video/tos/useast2a/tos-useast2a-ve-0068c002/a2d336414b744f45bed7a2d93e3cb495/?a=1233&amp;br=8014&amp;bt=4007&amp;cd=0%7C0%7C1&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;l=202106081244570101891941634905EDCD&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=M3hkcHBzbG1yNTMzNzczM0ApN2dpOWczZTtlN2VkMzszNmdhbDItYmptX21gLS1kMTZzczJhNmBgYy8uMDBeLzUyMTA6Yw%3D%3D&amp;vl=&amp;vr="
+        src="https://v77.tiktokcdn.com/4f5ccb30b965b96189e853d25a75fff8/60c10854/video/tos/useast2a/tos-useast2a-ve-0068c003/2060c02773eb4d5791c6fbd7fc64c03d/?a=1233&br=1982&bt=991&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20210609122827010189072215230D18E9&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anc2eTxobXU7NTMzNzczM0ApO2hkPGU6PGU6N2czNTo0NmdfcXJocXNmLjVgLS1kMTZzc2NeYF8vMi4zMDEuNi8wYTU6Yw%3D%3D&vl=&vr="
       ></video>
     </div>
   );
