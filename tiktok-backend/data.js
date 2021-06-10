2074068d2b767a2c70dad012273c5caecebad6ef
yash621 <yg17381@gmail.com>
2021-06-11 04:06:02 +0530

    proj comp

---
+++ tiktok-backend/data.js
@@ -0,0 +1,13 @@
+const data = [
+  {
+    url: "https://v77.tiktokcdn.com/ebb56a64b76049c2d04641698ed00f0e/60c2da81/video/tos/useast2a/tos-useast2a-ve-0068c001/ca0848c2919f4ee9a201e0f0d67f5b69/?a=1233&br=1786&bt=893&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20210610213700010190187163492892A3&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3l1ZDVvNDVqNTMzNzczM0ApOzs7ZTxkNTszN2k7N2g5ZGcuLTJsLjYtYzNgLS1kMTZzcy4tXzIzMV4tYl8yYjMvYWI6Yw%3D%3D&vl=&vr=",
+    description: "This is my life",
+    username: "jadoooo",
+    song: "Happy Pills",
+    likes: "845989",
+    comments: "485390",
+    shares: "45785",
+  },
+];
+
+export default data;
