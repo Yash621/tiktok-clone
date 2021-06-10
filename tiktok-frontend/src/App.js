1b40a90f390bda5aa52a017d5fa76dbbe7ef0497
yash621 <yg17381@gmail.com>
2021-06-11 03:28:20 +0530

    project completed

--- tiktok-frontend/src/App.js
+++ tiktok-frontend/src/App.js
@@ -1,24 +1,35 @@
 import "./App.css";
+import instance from "./axios.js";
 import Video from "./Video";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPost() {
+      const response = await instance.get("/v2/posts");
+      setVideos(response.data);
+      return response;
+    }
+
+    fetchPost();
+  }, []);
+
   return (
     <div className="App">
       <div className="app_videos">
-        <Video url={}
-          description={ }
-          username={ }
-          song={ }
-          likes={ }
-          comments={ }
-          shares={ }/>
-        <Video url={}
-          description={ }
-          username={ }
-          song={ }
-          likes={ }
-          comments={ }
-          shares={ }/>
+        {videos.map((video) => (
+          <Video
+            url={video.url}
+            description={video.description}
+            username={video.username}
+            song={video.song}
+            likes={video.likes}
+            comments={video.comments}
+            shares={video.shares}
+          />
+        ))}
       </div>
     </div>
   );
